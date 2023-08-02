9e7c7516d90c96fcd62ae0878fd012f0f65d6e11
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-02 15:41:34 +0530

    Add files via upload

---
+++ python code.py
@@ -0,0 +1,7 @@
+name = "heather "
+number = len(name) *9
+print("Hello " + name  + "your lucky number is "+ str(number))
+
+name = "sharon "
+number = len(name) *1
+print("hello " + name + "your lucky number is " + str(number))
