e0d6c183f2c075630b2d5c6a2cc88817ca9501ca
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-04 11:19:04 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,9 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = int(input("enter first number"))
+enter first number 150
+>>> num2 = int(input("enter second number"))
+enter second number70
+>>> result = num1/num2
+>>> print("result is : ", result)
+result is :  2.142857142857143
