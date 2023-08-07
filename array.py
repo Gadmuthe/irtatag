ffc8e895cd8692935fa7f224119546368f36813c
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-07 14:47:38 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,10 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from array import*
+>>> array1 = array('i', [1,2,3,4,5])
+>>> print(array1[0])
+1
+>>> print(array1[2])
+3
+>>> print(array1[4])
+5
