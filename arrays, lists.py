d7e2e4a70cb8075467282d0840fc7700bd88ce23
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-08 11:32:38 +0530

    Add files via upload

---
+++ arrays, lists.py
@@ -0,0 +1,28 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> [5,2,4,5,7,2]
+[5, 2, 4, 5, 7, 2]
+>>> ['bob','sue','john']
+['bob', 'sue', 'john']
+>>> a = [1,2,3,4]
+>>> names = ['bob','sue','john']
+>>> a
+[1, 2, 3, 4]
+>>> names
+['bob', 'sue', 'john']
+>>> a[2]
+3
+>>> a[0]
+1
+>>> a.append(8)
+>>> a
+[1, 2, 3, 4, 8]
+>>> a[4]
+8
+>>> a[2] = 10
+>>> a
+[1, 2, 10, 4, 8]
+>>> range(4,11)
+range(4, 11)
+>>> list(range(4,11))
+[4, 5, 6, 7, 8, 9, 10]
