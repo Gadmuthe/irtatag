faac49f994a7070c923d09b5cfe93c0fc1e37a0d
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-10 12:30:02 +0530

    Add files via upload

---
+++ intro to while loops.py
@@ -0,0 +1,19 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 0
+>>> while x < 5:
+...     print('not there yet, x=' +str(x))
+...     x = x + 1
+...     print('x=' + str(x))
+... 
+...     
+not there yet, x=0
+x=1
+not there yet, x=1
+x=2
+not there yet, x=2
+x=3
+not there yet, x=3
+x=4
+not there yet, x=4
+x=5
