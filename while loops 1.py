af3f34fdd3b1005a2e66c7d7015fc18b1da69d59
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-13 16:44:03 +0530

    Add files via upload

---
+++ while loops 1.py
@@ -0,0 +1,51 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from time import sleep
+n = 3
+while n >= 0:
+    print(n)
+    sleep(1)
+    n = n - 1
+
+    
+3
+2
+1
+0
+>>> 
+>>> 
+>>> from time import sleep
+>>> n = 3
+>>> while n >= 0:
+...     print(n)
+...     sleep(1)
+...     n = n + 1
+... 
+...     
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
