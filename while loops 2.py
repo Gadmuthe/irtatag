968bd53fc6b80b44760177c16c5758a1aa24d6fe
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-13 16:44:15 +0530

    Add files via upload

---
+++ while loops 2.py
@@ -0,0 +1,20 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> candy_purchased = 0
+>>> while candy_purchased <= 100:
+...     if candy_purchased % 10 == 0:
+...         print('Candy purchased: ' + str(candy_purchased))
+...     candy_purchased += 1
+... 
+...     
+Candy purchased: 0
+Candy purchased: 10
+Candy purchased: 20
+Candy purchased: 30
+Candy purchased: 40
+Candy purchased: 50
+Candy purchased: 60
+Candy purchased: 70
+Candy purchased: 80
+Candy purchased: 90
+Candy purchased: 100
