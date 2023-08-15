9cdbc05a1f3f391be487e6e02ad085ea1ead4862
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-15 23:34:25 +0530

    Add files via upload

---
+++ basic operators.py
@@ -0,0 +1,27 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> val1 = int(input("enter the value 1: "))
+enter the value 1: 51
+>>> val2 = int(input("enter the value 2: "))
+enter the value 2: 70
+>>> opr = input("enter the operator..(+,-,*,/)")
+enter the operator..(+,-,*,/)
+>>> if opr == "+":
+...     print(val1 + val2)
+... elif opr == "-":
+...     print(val1 - val2)
+... elif opr == "*":
+...     print(val1 * val2)
+... elif opr == "/":
+...     print(val1 / val2)
+... else:
+...     print("wrong input, program terminated")
+... 
+...     
+wrong input, program terminated
+>>> num1 = 51
+>>> num2 = 70
+>>> print("num1 * num2")
+num1 * num2
+>>> print(int(num1 * num2))
+3570
