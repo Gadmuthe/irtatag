89c27f02d826ba884490bbf87e69ff11647c84cf
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-16 16:01:02 +0530

    Add files via upload

---
+++ for loops 2.py
@@ -0,0 +1,19 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def score_counter(score_list):
+...     negative_scores = 0
+...     negative_scores = 0
+...     positive_scores = 0
+...     for score in score_list:
+...         if score>= 9:
+...             postive_scores += 1
+...         elif score >= 6:
+...             neutral_scores += 1
+...         else:
+...             negative_scores += 1
+...             print('negative:', negative_scores)
+...             print('neutral:', neutral_scores)
+...             print('positive:', positive_scores)
+... 
+...             
+>>> 
