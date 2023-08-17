7784b172aeabf6ca1829bf92b6ec1ea5b5662f49
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-17 17:00:14 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,16 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> greeting_1 = 'hello '
+>>> greeting_2 = 'world'
+>>> greeting_1 + greeting_2
+'hello world'
+>>> 
+>>> 
+>>> danger = 'Danger! '
+>>> danger * 3
+'Danger! Danger! Danger! '
+>>> 
+>>> 
+>>> quote = '"thank you for pressing the self-destruct button."'
+>>> print(quote)
+"thank you for pressing the self-destruct button."
