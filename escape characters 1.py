69d0dbc8017c6e729fc721cf9ec327535f671c3c
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-18 10:57:23 +0530

    Add files via upload

---
+++ escape characters 1.py
@@ -0,0 +1,51 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("line1 \
+... line2 \
+... line3")
+line1 line2 line3
+>>> 
+>>> 
+>>> print("\\")
+\
+>>> 
+>>> 
+>>> print('\'')
+'
+>>> 
+>>> 
+>>> print("\"")
+"
+>>> 
+>>> 
+>>> print("hello \a world")
+hello  world
+>>> 
+>>> 
+>>> print("hello \b world!")
+hello  world!
+>>> 
+>>> 
+>>> print("heelo \n world!")
+heelo 
+ world!
+>>> 
+>>> 
+>>> print("hello \r world!")
+hello 
+>>>  world!
+>>> 
+
+print("hello \t world!")
+hello 	 world!
+
+
+print("hello \v world!")
+hello 
+ world!
+
+
+print("\110\145\154\154\157\40\127\157\162\154\144\41")
+Hello World!
+
+
