60cfef2bb64b9bc721a90522969fbcebd0453f66
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-18 10:57:33 +0530

    Add files via upload

---
+++ escape characters 2.py
@@ -0,0 +1,29 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> quote = "\" its dangerous to go alone!\""
+>>> print(quote)
+" its dangerous to go alone!"
+>>> 
+>>> 
+>>> greeting = "good day, \nsir."
+>>> print(greeting)
+good day, 
+sir.
+>>> 
+>>> 
+>>> newline = "\\n represents a newline in python."
+>>> print(newline)
+\n represents a newline in python.
+>>> 
+>>> 
+>>> python = 'python'
+>>> for letter in python:
+...     print(letter + 'ut')
+... 
+...     
+put
+yut
+tut
+hut
+out
+nut
