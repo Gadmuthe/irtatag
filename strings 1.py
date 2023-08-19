1f5f2fff36ae03463d8a8827e55e8f5ad8d1dabf
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-19 13:26:29 +0530

    Add files via upload

---
+++ strings 1.py
@@ -0,0 +1,32 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> pets = 'cats and dogs'
+>>> pets.index('s')
+3
+>>> 
+>>> 
+>>> name = 'jolene'
+>>> name[0]
+'j'
+>>> name[5]
+'e'
+>>> 
+>>> 
+>>> sentence = 'a man, a plan, a canal, panama'
+>>> sentence[-1]
+'a'
+>>> 
+>>> 
+>>> color = 'orange'
+>>> color[1:4]
+'ran'
+>>> 
+>>> fruit = 'pineapple'
+>>> fruit[:4]
+'pine'
+>>> fruit[4:]
+'apple'
+>>> 'banana' in fruit
+False
+>>> 'apple' in fruit
+True
