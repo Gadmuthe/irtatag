d7e262451086124eedd7d70c202a18890b8b8dac
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-20 18:15:35 +0530

    Add files via upload

---
+++ swap two variables.py
@@ -0,0 +1,11 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 5
+>>> y = 10
+>>> temp = x
+>>> x = y
+>>> y = temp
+>>> print('the value of x after swapping: {}'.format(x))
+the value of x after swapping: 10
+>>> print('the value of y after swapping: {}'.format(y))
+the value of y after swapping: 5
