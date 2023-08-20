00b472b7e6f8e6637563116b45dc2641ddf1a1a4
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-20 18:15:47 +0530

    Add files via upload

---
+++ swap two variables 1.py
@@ -0,0 +1,9 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 5
+>>> y = 10
+>>> x, y = y, x
+>>> print("x =", x)
+x = 10
+>>> print("y =", y)
+y = 5
