245d6e5319480830f8b373a0f4877b6aacb5471c
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-22 16:53:47 +0530

    Add files via upload

---
+++ find sqaure root.py
@@ -0,0 +1,6 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num = 8
+>>> num_sqrt = num ** 0.5
+>>> print('the sqaure root of %o.3f is %0.3f'%(num, num_sqrt))
+the sqaure root of 10.3f is 2.828
