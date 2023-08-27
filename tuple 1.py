56792a5091f138be981a1e8bf56a19747f0e7c8a
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-27 18:12:10 +0530

    Add files via upload

---
+++ tuple 1.py
@@ -0,0 +1,27 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(list('rocks'))
+['r', 'o', 'c', 'k', 's']
+>>> print(list(('stones', 'water', 'underground')))
+['stones', 'water', 'underground']
+>>> 
+>>> 
+>>> num_list = [1, 2, 3]
+>>> num_list[0] = 5446
+>>> print(num_list)
+[5446, 2, 3]
+>>> 
+>>> 
+>>> test1 = (1)
+>>> test2 = (2,)
+>>> print(type(test1))
+<class 'int'>
+>>> print(type(test2))
+<class 'tuple'>
+>>> 
+>>> tuple1 = 1,
+>>> tuple2 = 2, 3
+>>> print(type(tuple1))
+<class 'tuple'>
+>>> print(type(tuple2))
+<class 'tuple'>
