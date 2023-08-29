e9ad121311f64b59ef06d50b101e6fe958693c1d
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-08-29 13:27:54 +0530

    Add files via upload

---
+++ dictionaries 1.py
@@ -0,0 +1,18 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> zoo = {
+...     'pen_1': 'penguins',
+...     'pen_2': 'zebras',
+...     'pen_3': 'lions',
+...     }
+>>> zoo['pen_2']
+'zebras'
+>>> zoo['pen_4'] = 'crocodiles'
+>>> print('zoo')
+zoo
+>>> zoo
+{'pen_1': 'penguins', 'pen_2': 'zebras', 'pen_3': 'lions', 'pen_4': 'crocodiles'}
+>>> print('pen_1' in zoo)
+True
+>>> print('pen_7' in zoo)
+False
