5a89b796c2275ad6b9b614903d5a28be83c8b92d
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-05 13:04:47 +0530

    Add files via upload

---
+++ negative if else.py
@@ -0,0 +1,14 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num = float(input("enter a number: "))
+enter a number: -70
+>>> if num >= 0:
+...     if num == 0:
+...         print("zero")
+...     else:
+...         print("positive number")
+... else:
+...     print("negative number")
+... 
+...     
+negative number
