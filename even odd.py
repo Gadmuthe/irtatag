86d9a2f476c591bae9d06066a013988f59b8b666
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-06 21:16:55 +0530

    Add files via upload

---
+++ even odd.py
@@ -0,0 +1,11 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num = int(input("enter a number: "))
+enter a number: 2509
+>>> if(num%2) == 0:
+...     print("{0} is even ".format(num))
+... else:
+...     print("{0} is odd".format(num))
+... 
+...     
+2509 is odd
