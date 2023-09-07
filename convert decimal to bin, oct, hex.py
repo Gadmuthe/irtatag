87994445ba55a4b4b48c93b22b7ad7090c591337
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-07 18:21:26 +0530

    Add files via upload

---
+++ convert decimal to bin, oct, hex.py
@@ -0,0 +1,22 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> dec = 344
+>>> print("the decimal value of", dec, "is:")
+the decimal value of 344 is:
+>>> print(bin(dec), "in binary.")
+0b101011000 in binary.
+>>> print(oct(dec), "in octal.")
+0o530 in octal.
+>>> print(hex(dec), "in hexadecimal.")
+0x158 in hexadecimal.
+>>> 
+>>> 
+>>> dec = 157
+>>> print("the decimal value of", dec, "is:")
+the decimal value of 157 is:
+>>> print(bin(dec), "in binary.")
+0b10011101 in binary.
+>>> print(oct(dec), "in octal.")
+0o235 in octal.
+>>> print(hex(dec), "in hexadecimal")
+0x9d in hexadecimal
