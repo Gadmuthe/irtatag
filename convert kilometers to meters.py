655b79ea3629b6ed94a2da31211278e8f88f3052
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-08 20:32:18 +0530

    Add files via upload

---
+++ convert kilometers to meters.py
@@ -0,0 +1,8 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> kilometers = float(input("enter the value in kilometers: "))
+enter the value in kilometers: 3.5
+>>> conv_fac = 0.621371
+>>> miles = kilometers * conv_fac
+>>> print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+3.50 kilometers is equal to 2.17 miles
