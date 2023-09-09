b50f1f9be6c66a1acab4e8b5a4248b92ca674c00
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-09 11:47:30 +0530

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,51 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num = 12
+>>> for i in range(1, 11):
+...     print(num, 'x',i, '=', num*i)
+... 
+...     
+12 x 1 = 12
+12 x 2 = 24
+12 x 3 = 36
+12 x 4 = 48
+12 x 5 = 60
+12 x 6 = 72
+12 x 7 = 84
+12 x 8 = 96
+12 x 9 = 108
+12 x 10 = 120
+>>> 
+>>> 
+>>> num = 7
+>>> for i in range(1,11):
+...     print(num, 'x',i, '=', num*i)
+... 
+...     
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
+>>> 
+>>> 
+>>> num = 15
+>>> for i in range(1, 11):
+...     print(num, 'x', i, '=', num*i)
+
+    
+15 x 1 = 15
+15 x 2 = 30
+15 x 3 = 45
+15 x 4 = 60
+15 x 5 = 75
+15 x 6 = 90
+15 x 7 = 105
+15 x 8 = 120
+15 x 9 = 135
+15 x 10 = 150
