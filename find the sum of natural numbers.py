fe060404accb8f13dbfa2c1d8db8de621e754ebb
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-12 18:24:15 +0530

    Add files via upload

---
+++ find the sum of natural numbers.py
@@ -0,0 +1,28 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num = 16
+>>> if num < 0:
+...     print("enter a positive number")
+... else:
+...     sum = 0
+...     while(num > 0):
+...         sum += num
+...         num -= 1
+...     print("the sum is", sum)
+... 
+...     
+the sum is 136
+>>> 
+>>> 
+>>> num = 157
+>>> if num < 0:
+...     print("enter a positive number")
+... else:
+...     sum = 0
+...     while(num > 0):
+...         sum += num
+...         num -= 1
+...     print("the sum is", sum)
+... 
+...     
+the sum is 12403
