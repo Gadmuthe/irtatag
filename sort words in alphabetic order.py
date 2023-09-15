31ff2a720c6a234152e15f15e9e36a301de8d82b
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-15 19:05:31 +0530

    Add files via upload

---
+++ sort words in alphabetic order.py
@@ -0,0 +1,19 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_str = "Hello this is an Example with cased letters"
+>>> words = [word.lower() for word in my_str.split()]
+>>> words.sort()
+>>> print("the sorted words are:")
+the sorted words are:
+>>> for word in words:
+...     print(word)
+... 
+...     
+an
+cased
+example
+hello
+is
+letters
+this
+with
