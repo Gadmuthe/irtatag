0c8421366ad6bf28baa07916c69fad8f86c5d7b7
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-28 13:16:26 +0530

    Add files via upload

---
+++ power of 2 using anonymous function.py
@@ -0,0 +1,130 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> terms = 10
+>>> result = list(map(lambda x: 2 ** x, range(terms)))
+>>> print("the total terms are:", terms)
+the total terms are: 10
+>>> for i in range(terms):
+...     print("2 raised to power", i, "is", result[i])
+... 
+...     
+2 raised to power 0 is 1
+2 raised to power 1 is 2
+2 raised to power 2 is 4
+2 raised to power 3 is 8
+2 raised to power 4 is 16
+2 raised to power 5 is 32
+2 raised to power 6 is 64
+2 raised to power 7 is 128
+2 raised to power 8 is 256
+2 raised to power 9 is 512
+>>> 
+>>> 
+>>> terms = 100
+>>> result = list(map(lambda x: 2 ** x, range(terms)))
+>>> print("the total terms are:", terms)
+the total terms are: 100
+>>> for i in range(terms):
+...     print("2 raised to power", i, "is", result[i])
+... 
+...     
+2 raised to power 0 is 1
+2 raised to power 1 is 2
+2 raised to power 2 is 4
+2 raised to power 3 is 8
+2 raised to power 4 is 16
+2 raised to power 5 is 32
+2 raised to power 6 is 64
+2 raised to power 7 is 128
+2 raised to power 8 is 256
+2 raised to power 9 is 512
+2 raised to power 10 is 1024
+2 raised to power 11 is 2048
+2 raised to power 12 is 4096
+2 raised to power 13 is 8192
+2 raised to power 14 is 16384
+2 raised to power 15 is 32768
+2 raised to power 16 is 65536
+2 raised to power 17 is 131072
+2 raised to power 18 is 262144
+2 raised to power 19 is 524288
+2 raised to power 20 is 1048576
+2 raised to power 21 is 2097152
+2 raised to power 22 is 4194304
+2 raised to power 23 is 8388608
+2 raised to power 24 is 16777216
+2 raised to power 25 is 33554432
+2 raised to power 26 is 67108864
+2 raised to power 27 is 134217728
+2 raised to power 28 is 268435456
+2 raised to power 29 is 536870912
+2 raised to power 30 is 1073741824
+2 raised to power 31 is 2147483648
+2 raised to power 32 is 4294967296
+2 raised to power 33 is 8589934592
+2 raised to power 34 is 17179869184
+2 raised to power 35 is 34359738368
+2 raised to power 36 is 68719476736
+2 raised to power 37 is 137438953472
+2 raised to power 38 is 274877906944
+2 raised to power 39 is 549755813888
+2 raised to power 40 is 1099511627776
+2 raised to power 41 is 2199023255552
+2 raised to power 42 is 4398046511104
+2 raised to power 43 is 8796093022208
+2 raised to power 44 is 17592186044416
+2 raised to power 45 is 35184372088832
+2 raised to power 46 is 70368744177664
+2 raised to power 47 is 140737488355328
+2 raised to power 48 is 281474976710656
+2 raised to power 49 is 562949953421312
+2 raised to power 50 is 1125899906842624
+2 raised to power 51 is 2251799813685248
+2 raised to power 52 is 4503599627370496
+2 raised to power 53 is 9007199254740992
+2 raised to power 54 is 18014398509481984
+2 raised to power 55 is 36028797018963968
+2 raised to power 56 is 72057594037927936
+2 raised to power 57 is 144115188075855872
+2 raised to power 58 is 288230376151711744
+2 raised to power 59 is 576460752303423488
+2 raised to power 60 is 1152921504606846976
+2 raised to power 61 is 2305843009213693952
+2 raised to power 62 is 4611686018427387904
+2 raised to power 63 is 9223372036854775808
+2 raised to power 64 is 18446744073709551616
+2 raised to power 65 is 36893488147419103232
+2 raised to power 66 is 73786976294838206464
+2 raised to power 67 is 147573952589676412928
+2 raised to power 68 is 295147905179352825856
+2 raised to power 69 is 590295810358705651712
+2 raised to power 70 is 1180591620717411303424
+2 raised to power 71 is 2361183241434822606848
+2 raised to power 72 is 4722366482869645213696
+2 raised to power 73 is 9444732965739290427392
+2 raised to power 74 is 18889465931478580854784
+2 raised to power 75 is 37778931862957161709568
+2 raised to power 76 is 75557863725914323419136
+2 raised to power 77 is 151115727451828646838272
+2 raised to power 78 is 302231454903657293676544
+2 raised to power 79 is 604462909807314587353088
+2 raised to power 80 is 1208925819614629174706176
+2 raised to power 81 is 2417851639229258349412352
+2 raised to power 82 is 4835703278458516698824704
+2 raised to power 83 is 9671406556917033397649408
+2 raised to power 84 is 19342813113834066795298816
+2 raised to power 85 is 38685626227668133590597632
+2 raised to power 86 is 77371252455336267181195264
+2 raised to power 87 is 154742504910672534362390528
+2 raised to power 88 is 309485009821345068724781056
+2 raised to power 89 is 618970019642690137449562112
+2 raised to power 90 is 1237940039285380274899124224
+2 raised to power 91 is 2475880078570760549798248448
+2 raised to power 92 is 4951760157141521099596496896
+2 raised to power 93 is 9903520314283042199192993792
+2 raised to power 94 is 19807040628566084398385987584
+2 raised to power 95 is 39614081257132168796771975168
+2 raised to power 96 is 79228162514264337593543950336
+2 raised to power 97 is 158456325028528675187087900672
+2 raised to power 98 is 316912650057057350374175801344
+2 raised to power 99 is 633825300114114700748351602688
