54ffbf71168190283105f2f812b5879c332c55a7
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-09-29 13:51:45 +0530

    Add files via upload

---
+++ check if a list is empty.py
@@ -0,0 +1,24 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_list = []
+>>> if not my_list:
+...     print("the list is empty")
+... 
+...     
+the list is empty
+>>> 
+>>> 
+>>> y_list = []
+>>> if not len(y_list):
+...     print("the list is empty")
+... 
+...     
+the list is empty
+>>> 
+>>> 
+>>> my_list = []
+>>> if my_list == []:
+...     print("the list is empty")
+... 
+...     
+the list is empty
