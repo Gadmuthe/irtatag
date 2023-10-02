7a628d536e77d94bac22f2be50b086639efcc659
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-10-02 12:44:49 +0530

    Add files via upload

---
+++ program to slice lists.py
@@ -0,0 +1,35 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_list = [1,2,3,4,5]
+>>> print(my_list[:])
+[1, 2, 3, 4, 5]
+>>> 
+>>> 
+>>> my_list = [1,2,3,4,5]
+>>> print(my_list[2:])
+[3, 4, 5]
+>>> 
+>>> 
+>>> my_list = [1,2,3,4,5]
+>>> print(my_list[:2])
+[1, 2]
+>>> 
+>>> 
+>>> my_list = [1,2,3,4,5]
+>>> print(my_list[2:4])
+[3, 4]
+>>> 
+>>> 
+>>> my_list = [1,2,3,4,5]
+>>> print(my_list[::2])
+[1, 3, 5]
+>>> 
+>>> 
+>>> my_list = [1,2,3,4,5]
+>>> print(my_list[::-2])
+[5, 3, 1]
+>>> 
+>>> 
+>>> my_list = [1,2,3,4,5]
+>>> print(my_list[1:4:2])
+[2, 4]
