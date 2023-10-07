ff117f948e104ba322a89df3c5ada90d56b84a2c
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-10-07 22:25:11 +0530

    Add files via upload

---
+++ check if a key is already present in a dictionary.py
@@ -0,0 +1,8 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_dict = {1: 'a', 2: 'b', 3: 'c'}
+>>> if 2 in my_dict:
+...     print("present")
+... 
+...     
+present
