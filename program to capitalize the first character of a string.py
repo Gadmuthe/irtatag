4df01a27556e5db5c99053d79231bcc35f2d2051
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-10-10 21:11:18 +0530

    Add files via upload

---
+++ program to capitalize the first character of a string.py
@@ -0,0 +1,11 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_string = "programiz is Lit"
+>>> print(my_string[0].upper() + my_string[1:])
+Programiz is Lit
+>>> 
+>>> 
+>>> my_string = "programiz is lit"
+>>> cap_string = my_string.capitalize()
+>>> print(cap_string)
+Programiz is lit
