a7ab38db1eaf8362f0453d222da9d9a79634f649
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-10-11 23:59:53 +0530

    Add files via upload

---
+++ random code.py
@@ -0,0 +1,37 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(1,11):
+...     print(i)
+... 
+...     
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+>>> 
+>>> 
+>>> num = int(input("enter a number: "))
+enter a number: 15777
+>>> if num % 2 == 0:
+...     print(f"{num}is even.")
+... else:
+...     print(f"{num} is odd.")
+... 
+...     
+15777 is odd.
+>>> 
+>>> 
+>>> length = float(input("enter the length of the rectangle: "))
+enter the length of the rectangle: 15
+>>> width = float(input("enter the width of the rectangle: "))
+enter the width of the rectangle: 77
+>>> area = length * width
+>>> print(f"the area of the rectangle with length {length} and width {width} is {area}.")
+the area of the rectangle with length 15.0 and width 77.0 is 1155.0.
+>>> 
