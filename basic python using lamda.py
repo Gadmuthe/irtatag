a5cb84e31dce937ff8190e5d49f34b2a31bf8ff6
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2023-10-15 22:26:35 +0530

    Add files via upload

---
+++ basic python using lamda.py
@@ -0,0 +1,15 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = lambda a: a+10
+>>> print(x(5))
+15
+>>> 
+>>> 
+>>> x = lambda a, b: a*b
+>>> print(x(5, 6))
+30
+>>> 
+>>> 
+>>> x = lambda a, b, c: a + b + c
+>>> print(x(5,6,2))
+13
